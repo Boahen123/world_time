31d55d238c72c9a85d1aaa6287a6b5e953443a15
Papa Kofi <humblepapa1234@gmail.com>
2023-06-18 23:20:51 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:world_clock/screens/home.dart';
+import 'package:world_clock/screens/loading_screen.dart';
+import 'package:world_clock/screens/select_location.dart';
+
+void main() {
+  // Define your named routes
+  final Map<String, Widget Function(BuildContext)> routes =
+      <String, WidgetBuilder>{
+    '/': (BuildContext context) => const Loading(),
+    '/home': (BuildContext context) => const Home(),
+    '/location': (BuildContext context) => const SelectLocation(),
+  };
+
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: routes,
+  ));
+}
