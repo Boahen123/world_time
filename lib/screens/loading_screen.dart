85e16fc61755410650a97a113e56622aea5124da
Papa Kofi <humblepapa1234@gmail.com>
2023-06-19 04:04:51 +0000

    Make HtTP get requests(loading_screen file)

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+import 'dart:convert';
+
+var console = Logger();
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +13,22 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getData() async {
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+    var response = await http.get(url);
+    const JsonDecoder decoder = JsonDecoder();
+
+    final Map<String, dynamic> object = decoder.convert(response.body);
+
+    console.d(object['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(body: Text('loading screen'));
--- lib/screens/select_location.dart
+++ lib/screens/select_location.dart
@@ -11,37 +11,13 @@ class SelectLocation extends StatefulWidget {
 class _SelectLocationState extends State<SelectLocation> {
   var console = Logger();
 
-  // simulating an asynchrounous network request
-  void getData() async {
-    await Future.delayed(
-      const Duration(seconds: 5),
-      () {
-        console.i('first statement');
-      },
-    );
-
-    Future.delayed(const Duration(seconds: 2), () {
-      console.i('second statement');
-    });
-
-    console.d('synchrounous statement');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
   @override
   Widget build(BuildContext context) {
-    // console.i('build function ran');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Select a Location'),
         centerTitle: true,
         backgroundColor: Colors.blue[900],
-        // elevation: 0.0,
       ),
     );
   }
